3d028ed5d0f4bfa704030ce184a7a673468196a9
Jason Schmitt <schmitty890@gmail.com>
2018-06-28 22:45:48 -0400

    adding models

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 var mongoose = require("mongoose");
 var cheerio = require("cheerio");
+var request = require("request");
 
 // require models
 // var db = require("./models");
@@ -19,9 +20,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // express.static serves the public folder as a static directory
 app.use(express.static("public"));
 
+// set up app to use handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Routes
 app.get("/", function(req, res) {
-  console.log('hookin it up');
+  console.log('hookin it upp');
 });
 
 // Start the server
